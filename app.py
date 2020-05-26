158c417bbce1a41e7b32c09ff964a5c98ffda014
Maja-Jer <maja.jercic@gmail.com>
2020-05-26 22:00:11 +0000

    finsihed setting to update category in DB + set up Delete category

--- app.py
+++ app.py
@@ -79,6 +79,11 @@ def update_category(category_id):
         {'category_name': request.form.get('category_name')})
     return redirect(url_for('get_categories'))
 
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=os.environ.get('PORT', '5000'),
