5314f45acd2d4161e64c1c06f6eed106d1fade1c
Maja-Jer <maja.jercic@gmail.com>
2020-05-26 22:25:50 +0000

    completed add category

--- app.py
+++ app.py
@@ -84,6 +84,17 @@ def delete_category(category_id):
     mongo.db.categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
 
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=os.environ.get('PORT', '5000'),
