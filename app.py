99003b38578f6ab02e1c5f419ea6ca8b4e99cc5d
Maja-Jer <maja.jercic@gmail.com>
2020-05-26 20:07:00 +0000

    changed DB names in Mongo and updated in the ccode throughout

--- app.py
+++ app.py
@@ -9,53 +9,62 @@ app.config["MONGO_DBNAME"] = 'Travel-O-Rama'
 app.config["MONGO_URI"] = 'mongodb+srv://starting_point:Maja-page@cluster0-n1tx1.mongodb.net/Travel-O-Rama?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
-for i in mongo.db.trip_type.find():
-    print(i)
 
 @app.route('/')
-@app.route('/get_trip_types')
-def get_trip_types():
-    print(mongo.db.trip_type.find())
-    return render_template("trips.html", trip_types=mongo.db.trip_type.find())
+@app.route('/get_trips')
+def get_trips():
+    return render_template("trips.html", 
+                           trips=mongo.db.trips.find())
+
 
 @app.route('/add_trip')
 def add_trip():
-    return render_template('addtrip.html', trip_type=mongo.db.trip_type.find())
+    return render_template('addtrip.html',
+                           categories=mongo.db.categories.find())
+
 
 @app.route('/insert_trip', methods=['POST'])
 def insert_trip():
-    trips = mongo.db.trip_type
+    trips =  mongo.db.trips
     trips.insert_one(request.form.to_dict())
     return redirect(url_for('get_trips'))
 
-#@app.route('/edit_trip/<trip_type_id>')
-#def edit_trip(trip_type_id):
-    #the_trip_type =  mongo.db.trip_types.find_one({"_id": ObjectId(trip_type_id)})
-    #return render_template('edittriptype.html', trip_type=the_trip_type)
 
-@app.route('/edit_trip/<trip_type_id>')
-def edit_trip(trip_type_id):
-    the_trip =  mongo.db.trips.find_one({"_id": ObjectId(trip_type_id)})
-    all_trip_types =  mongo.db.trip_type.find()
+@app.route('/edit_trip/<trip_id>')
+def edit_trip(trip_id):
+    the_trip =  mongo.db.trips.find_one({"_id": ObjectId(trip_id)})
+    all_categories =  mongo.db.categories.find()
     return render_template('edittrip.html', trip=the_trip,
-                trip_types=all_trip_types)
+                           categories=all_categories)
 
-@app.route('/update_trip/<trip_type_id>', methods=["POST"])
-def update_trip(trip_type_id):
-    trips = mongo.db.trip_type
-    trips.update( {'_id': ObjectId(trip__type_id)},
+
+@app.route('/update_trip/<trip_id>', methods=["POST"])
+def update_trip(trip_id):
+    trips = mongo.db.trips
+    trips.update( {'_id': ObjectId(trip_id)},
     {
-        'trip_type':request.form.get('trip_type'),
-        'trip_location':request.form.get('trip_location'),
+        'trip_location': request.form.get('trip_location'),
         'transport_code': request.form.get('transport_code'),
         'check_in': request.form.get('check_in'),
-        'check_out':request.form.get('check_out'),
-        'is_now':request.form.get('is_now'),
-        'sights':request.form.get('sights'),
-        'restaurants':request.form.get('restaurants'),
-        'activities':request.form.get('activities')
+        'check_out': request.form.get('check_out'),
+        'is_now': request.form.get('is_now'),
+        'sights': request.form.get('sights'),
+        'restaurants': request.form.get('restaurants'),
+        'activities': request.form.get('activities')
     })
     return redirect(url_for('get_trips'))
+
+
+@app.route('/delete_trip/<trip_id>')
+def delete_trip(trip_id):
+    mongo.db.trips.remove({'_id': ObjectId(trip_id)})
+    return redirect(url_for('get_trips'))
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
