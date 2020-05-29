108747f5b62552a041737e09cd2bcc55eb108049
Maja-Jer <maja.jercic@gmail.com>
2020-05-29 18:56:19 +0000

    Fixed small bugs in css, font sizing in Navbar, media query for button size in trips.html, wasn't able to override default css for the font size in labels in trips.html as well as adjusting the margin in moble quesry (commented on the code)

--- app.py
+++ app.py
@@ -10,22 +10,23 @@ app.config["MONGO_URI"] = 'mongodb+srv://starting_point:Maja-page@cluster0-n1tx1
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/get_trips')
 def get_trips():
-    return render_template("trips.html", 
-    trips=mongo.db.trips.find())
+    return render_template("trips.html",
+                           trips=mongo.db.trips.find())
 
 
 @app.route('/add_trip')
 def add_trip():
     return render_template('addtrip.html',
-    categories=mongo.db.categories.find())
+                           categories=mongo.db.categories.find())
 
 
 @app.route('/insert_trip', methods=['POST'])
 def insert_trip():
-    trips =  mongo.db.trips
+    trips = mongo.db.trips
     print(request.form)
     trips.insert_one(request.form.to_dict())
     return redirect(url_for('get_trips'))
@@ -33,8 +34,8 @@ def insert_trip():
 
 @app.route('/edit_trip/<trip_id>')
 def edit_trip(trip_id):
-    the_trip =  mongo.db.trips.find_one({"_id": ObjectId(trip_id)})
-    all_categories =  mongo.db.categories.find()
+    the_trip = mongo.db.trips.find_one({"_id": ObjectId(trip_id)})
+    all_categories = mongo.db.categories.find()
     return render_template('edittrip.html', trip=the_trip,
                            categories=all_categories)
 
@@ -42,8 +43,8 @@ def edit_trip(trip_id):
 @app.route('/update_trip/<trip_id>', methods=["POST"])
 def update_trip(trip_id):
     trips = mongo.db.trips
-    trips.update( {'_id': ObjectId(trip_id)},
-    {
+    trips.update({'_id': ObjectId(trip_id)},
+                 {
         'trip_location': request.form.get('trip_location'),
         'transport_code': request.form.get('transport_code'),
         'check_in': request.form.get('check_in'),
@@ -65,13 +66,15 @@ def delete_trip(trip_id):
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html',
-    categories=mongo.db.categories.find())
+                           categories=mongo.db.categories.find())
+
 
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     return render_template('editcategory.html',
-    category=mongo.db.categories.find_one(
-    {'_id': ObjectId(category_id)}))
+                           category=mongo.db.categories.find_one(
+                               {'_id': ObjectId(category_id)}))
+
 
 @app.route('/update_category/<category_id>', methods=['POST'])
 def update_category(category_id):
@@ -80,10 +83,12 @@ def update_category(category_id):
         {'category_name': request.form.get('category_name')})
     return redirect(url_for('get_categories'))
 
+
 @app.route('/delete_category/<category_id>')
 def delete_category(category_id):
     mongo.db.categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
+
 
 @app.route('/insert_category', methods=['POST'])
 def insert_category():
@@ -95,6 +100,7 @@ def insert_category():
 @app.route('/add_category')
 def add_category():
     return render_template('addcategory.html')
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
